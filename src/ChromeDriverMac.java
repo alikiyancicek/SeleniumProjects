85f2308aea9ed05217b8a6c74042e6cd4e1bc091
Ali Kiyancicek <ali.kiyancicek@gmail.com>
2022-07-04 23:47:06 -0400

    chrome driver file modified

--- src/ChromeDriverMac.java
+++ src/ChromeDriverMac.java
@@ -1,5 +1,8 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class ChromeDriverMac {
@@ -10,17 +13,32 @@ public class ChromeDriverMac {
         // if we copy drivers into the /usr/local/bin directory, we do not have to write line 14.
         // if the driver changed, we need to copy and paste new driver for this location.
 
-        // specifing location of driver into our local machine
+        // specifying location of driver into our local machine
         System.setProperty("webdriver.chrome.driver","/Users/alikiyancicek/Dev/WebDrivers/chromedriver");
 
-        // initialization of new chromedriver object
+        // initialization of new chromedriver
         WebDriver driver = new ChromeDriver();
 
-        // specifiying the url that we wanted to open
-        String baseURL = "https://letskodeit.com";
+        // specifying the url that we wanted to open
+        String baseURL = "https://letskodeit.teachable.com/";
 
-        //openning the specified URL on the specified browser
+        // maximize webpage maximum size
+        driver.manage().window().maximize();
+
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        //opening the specified URL on the specified browser
         driver.get(baseURL);
 
+        //find element and click it. - this element was log in page button.
+        driver.findElement(By.xpath("//*[@id=\"navbar\"]/div/div/div/ul/li[2]/a")).click();
+
+        // find email login (input)) by xpath  and fill it with email.
+        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("ali@gmail.com");
+
+        // send password information via the same method
+        driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("123456");
+
+        //*[@id="password"]
     }
 }
