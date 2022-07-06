98509d8cea23253e5d7f929974f4315dee861fb2
Ali Kiyancicek <ali.kiyancicek@gmail.com>
2022-07-05 23:05:54 -0400

    Find element by tagname

---
+++ src/FindElementByTagName.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class FindElementByTagName {
+
+    public static void main(String[] args) {
+
+        // not videly use because there is many same named tag names.like anchor tag, div tag etc.
+
+        System.setProperty("webdriver.edge.driver","/Users/alikiyancicek/Dev/WebDrivers/msedgedriver");
+        WebDriver driver = new EdgeDriver();
+        String baseURL = "https://letskodeit.com";
+        driver.get(baseURL);
+
+        driver.findElement(By.tagName("a")).click();
+
+    }
+}
