b81f54ed116fdeda6568b5adfcd1524397cd66ae
Ali Kiyancicek <ali.kiyancicek@gmail.com>
2022-07-06 23:05:46 -0400

    to new branch

---
+++ src/AdvancedSelectors/ChromeDriverMac2.java
@@ -0,0 +1,39 @@
+package AdvancedSelectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class ChromeDriverMac2 {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","/Users/alikiyancicek/Dev/WebDrivers/chromedriver");
+        // initialization of new chromedriver
+        WebDriver driver = new ChromeDriver();
+        // specifying the url that we wanted to open
+        String baseURL = "https://letskodeit.teachable.com/";
+        // maximize webpage maximum size
+        driver.manage().window().maximize();
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        //opening the specified URL on the specified browser
+        driver.get(baseURL);
+
+        //find element and click it. - this element was log in page button.
+        driver.findElement(By.xpath("//*[@id=\"navbar\"]/div/div/div/ul/li[2]/a")).click();
+
+        // find email login (input)) by xpath  and fill it with email.
+        driver.findElement(By.xpath("//*[@id=\"email\"]")).sendKeys("ali@gmail.com");
+
+        // send password information via the same method
+        driver.findElement(By.name("password")).sendKeys("123456");
+
+        //find element and click it. - this element was log in button.
+        driver.findElement(By.xpath("/html/body/main/div/form/div[4]/input")).click();
+
+    }
+}
